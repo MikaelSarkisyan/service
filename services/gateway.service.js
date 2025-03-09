645b67d1df3200e1656187f87ab69f3c9abadcc6
Mikael Sarkisyan <miqosarkisyan@mail.ru>
2025-03-09 19:28:27 +0400

    init

--- services/gateway.service.js
+++ services/gateway.service.js
@@ -1,5 +1,6 @@
+import moleculerWeb from 'moleculer-web';
+const HTTPServer = moleculerWeb.HTTPServer;
 import { ServiceBroker } from "moleculer";
-import { HTTPServer } from "moleculer-web";
 
 const broker = new ServiceBroker({
   nodeID: "node-1",
